d577ada251efd8f6e0c68c5fc0c7a33167bda2aa
GunagyeJain <64782892+GunagyeJain@users.noreply.github.com>
2025-07-21 22:21:22 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCP2y34kf6xNNzICYOX8foVUcX0MvD3jTA",
-  authDomain: "expense-tracker-8c0ef.firebaseapp.com",
-  projectId: "expense-tracker-8c0ef",
-  storageBucket: "expense-tracker-8c0ef.firebasestorage.app",
-  messagingSenderId: "549334192581",
-  appId: "1:549334192581:web:296c894b50c10d9f803bac",
-  measurementId: "G-NNB5T7BNZZ"
+// ADD firebase data
 };
 
 // Initialize Firebase
